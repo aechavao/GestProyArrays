cc95e08d2730df0283c8c3cd1fdc03b70f51e9fe
aetxabao <aitor.etxabarren@mariaanasanz.es>
2025-12-17 11:58:30 +0100

    main Trabajador

--- src/main/java/Trabajador.java
+++ src/main/java/Trabajador.java
@@ -46,7 +46,7 @@ public class Trabajador {
     public static void main(String[] args) {
         Trabajador t1 = new Trabajador("Aitor");
         System.out.println(t1);
-        Trabajador t2 = new Trabajador("Javi", Ctes.CAT_PRACTICAS);
+        Trabajador t2 = new Trabajador("Alba", Ctes.CAT_PRACTICAS);
         System.out.println(t2);
     }
 
